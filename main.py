44abdb9825f2800644e1d7597cd817ccb537962b
Maelys Gaido <maelys.gaido@le-campus-numerique.fr>
2022-05-30 16:02:03 +0200

    test back8

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello you"}
 
+
 @app.post("/")
 async def test():
 
-     return "ok buddy"
+
+    return "ok buddy"
